df31464d74690bbbb446bf435723eacf2d363073
ashrafstakala <ashrafstakala@gmail.com>
2021-12-23 18:36:21 +0200

    added entry form

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends React.Component {
     manager: '',
     players: [],
     balance: '',
+    value: '',
   };
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
@@ -26,6 +27,19 @@ class App extends React.Component {
           {this.state.players.length} people competing to win{' '}
           {web3.utils.fromWei(this.state.balance, 'ether')} ether!
         </p>
+        <hr />
+
+        <form>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <button>Enter</button>
+        </form>
       </div>
     );
   }
